f4e0d8ccd2284f4ce5e91a833e20f7ffd1b32e2b
mariobp <mariobarrpach@gmail.com>
2017-05-10 19:28:47 -0400

    dependencias

--- geoActivos/settings.py
+++ geoActivos/settings.py
@@ -84,13 +84,13 @@ WSGI_APPLICATION = 'geoActivos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'geoactivos',
+        'NAME': 'geo',
         'USER': 'postgres',
-        'PASSWORD': '123456',
+        'PASSWORD': 'Exile*74522547',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -103,6 +103,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'geo.db'),
     }
 }
+"""
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -155,12 +156,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-DJANGOCMS_YOUTUBE_API_KEY = "AIzaSyDWInVLy5ZsqI613jqOpESM7qI4OqRSfi4"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/' 
+STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, '../media/')
+MEDIA_ROOT = '/home/mario/geoactivos/media/'
